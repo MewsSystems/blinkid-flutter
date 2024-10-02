15a39ea6c06d9ae5b9a5949e6a11a59f04bdad04
Vlad Alboiu <alboiuvlad29@gmail.com>
2024-10-02 14:39:39 +0100

    upd

--- BlinkID/example/lib/main.dart
+++ BlinkID/example/lib/main.dart
@@ -9,12 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Microblink Demo',
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
